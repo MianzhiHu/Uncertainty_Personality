e660b4107fe0df463ba6bd4840f23776da5c581c
Rudolf Hu <zuirenlangzi@gmail.com>
2024-01-16 22:10:54 -0600

    1/16

--- modeling.py
+++ modeling.py
@@ -47,7 +47,24 @@ print(likelihood_ratio_test(decay, decayfre, 1))
 print(bayes_factor(decay, decayfre))
 
 # get the best beta
+t = best_param_generator(decayfre, 't')
+a = best_param_generator(decayfre, 'a')
 beta = best_param_generator(decayfre, 'b')
+
+for parameter in [t, a, beta]:
+    for i, value in enumerate(parameter):
+        if value == 1:
+            # change the value to 0.9999
+            parameter[i] = 0.9999
+        elif value == 0:
+            # change the value to 0.0001
+            parameter[i] = 0.0001
+
+# stack the best parameters to the data
+CA.loc[:, 't'] = t
+CA.loc[:, 'a'] = a
+CA.loc[:, 'b'] = beta
+CA.to_csv('./Data/CA_para.csv', index=False)
 
 # get the correlation between the best beta and the PropOptimal
 print(pearsonr(CA['Picking C'], beta))
@@ -59,8 +76,16 @@ CA_A2 = CA[CA['Condition'] == 'S2A2']
 # get the correlation between the best beta and the PropOptimal
 beta_A1 = beta[CA_A1.index]
 beta_A2 = beta[CA_A2.index]
+t_A1 = t[CA_A1.index]
+t_A2 = t[CA_A2.index]
+a_A1 = a[CA_A1.index]
+a_A2 = a[CA_A2.index]
 
 print(pearsonr(CA_A1['Picking C'], beta_A1))
 print(pearsonr(CA_A2['Picking C'], beta_A2))
 
+for parameter in [t_A1, a_A1]:
+    print(pearsonr(CA_A1['Picking C'], parameter))
+for parameter in [t_A2, a_A2]:
+    print(pearsonr(CA_A2['Picking C'], parameter))
 
